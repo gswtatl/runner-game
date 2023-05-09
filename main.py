a3832e61064b46ad5ead8bd7535a99dc7bb0de39
nxmirich <nxmirich@gmail.com>
2023-05-09 23:23:21 +0300

    made rectangles for player and snail

--- main.py
+++ main.py
@@ -3,38 +3,38 @@ pygame.init()
 
 win = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
-sky_surface = pygame.image.load("graphics/Sky.png").convert_alpha()
-ground_surface = pygame.image.load("graphics/ground.png").convert_alpha()
-snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-player_surface = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
+clock = pygame.time.Clock()
+
+sky_surf = pygame.image.load("graphics/Sky.png").convert_alpha()
+ground_surf = pygame.image.load("graphics/ground.png").convert_alpha()
+
+snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom=(700, 300))
+
 text = pygame.font.Font("font/Pixeltype.ttf", 50)
 
-
-def main():
-    clock = pygame.time.Clock()
-    run = True
-    snail_x_pos = 700
-
-    while run:
-        clock.tick(60)
-
-        win.blit(sky_surface, (0, 0))
-        win.blit(ground_surface, (0, 300))
-        win.blit(text.render("Runner", False, (0, 0, 0)), (345, 70))
-        snail_x_pos -= 4
-        if snail_x_pos < -100:
-            snail_x_pos = 900
-        win.blit(snail_surface, (snail_x_pos, 265))
-        pygame.display.update()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                break
-
-    pygame.quit()
-    quit()
+player_surf = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom=(80, 300))
 
 
-if __name__ == "__main__":
-    main()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    clock.tick(60)
+
+    win.blit(sky_surf, (0, 0))
+    win.blit(ground_surf, (0, 300))
+    win.blit(text.render("Runner", False, (0, 0, 0)), (345, 70))
+
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    win.blit(snail_surf, snail_rect)
+
+    win.blit(player_surf, player_rect)
+
+    pygame.display.update()
+
