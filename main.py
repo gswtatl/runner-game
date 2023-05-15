a4968d57ca5627d451a06db8821736139ad59843
nxmirich <nxmirich@gmail.com>
2023-05-15 12:55:06 +0300

    almost done class

--- main.py
+++ main.py
@@ -2,6 +2,44 @@ import pygame
 from random import randint
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        player_walk_1 = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
+        player_walk_2 = pygame.image.load("graphics/Player/player_walk_2.png").convert_alpha()
+        self.player_walk = [player_walk_1, player_walk_2]
+        self.player_index = 0
+        self.player_jump = pygame.image.load("graphics/Player/jump.png").convert_alpha()
+
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom=(200, 300))
+        self.gravity = 0
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
+            self.gravity = -20
+
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 300:
+            self.rect.bottom = 300
+
+    def animation_state(self):
+        if self.rect.bottom < 300:
+            self.image = self.player_jump
+        else:
+            self.player_index += 0.1
+            if self.player_index >= len(self.player_walk):
+                self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+
+
 def display_score():
     current_time = int(pygame.time.get_ticks()/1000) - start_time
     score_surf = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
@@ -56,6 +94,9 @@ test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 game_active = False
 start_time = 0
 score = 0
+
+player = pygame.sprite.GroupSingle()
+player.add(Player())
 
 sky_surf = pygame.image.load("graphics/Sky.png").convert_alpha()
 ground_surf = pygame.image.load("graphics/ground.png").convert_alpha()
@@ -161,6 +202,8 @@ while True:
             player_rect.bottom = 300
         player_animation()
         win.blit(player_surf, player_rect)
+        player.draw(win)
+        player.update()
 
         # obstacle movement
         obstacle_rect_list = obstacle_movement(obstacle_rect_list)
